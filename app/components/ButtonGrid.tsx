9792d052a2aa9369f1e0f0bd74ee9e9b5ebaba96
philipp939 <schneiderphilipp27@icloud.com>
2025-02-15 17:58:18 +0100

    Add files via upload

--- app/components/ButtonGrid.tsx
+++ app/components/ButtonGrid.tsx
@@ -6,17 +6,12 @@ import ModpackButton from "./ModpackButton"
 
 export default function ButtonGrid() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 w-full max-w-4xl">
-      <div className="col-span-1 sm:col-span-2 md:col-span-3">
-        <DiscordButton />
-      </div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 w-full max-w-4xl mx-auto place-items-center">
+      <DiscordButton />
       <RulesButton />
       <CommandsButton />
       <KeybindsButton />
-      <div className="col-span-1 sm:col-span-2 md:col-span-3">
-        <ModpackButton />
-      </div>
+      <ModpackButton />
     </div>
   )
 }
-
