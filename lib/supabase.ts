96c7c7a5bc6750082e86a6f3cbe1f37dd56b28a0
philipp939 <schneiderphilipp27@icloud.com>
2025-05-19 15:30:48 +0200

    Add files via upload

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,8 +2,10 @@ import { createClient } from "@supabase/supabase-js"
 
 // Für serverseitige Aufrufe
 export const createServerSupabaseClient = () => {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ""
-  const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || ""
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://vefhpdjiqxugnykucfep.supabase.co"
+  const supabaseKey =
+    process.env.SUPABASE_SERVICE_ROLE_KEY ||
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZlZmhwZGppcXh1Z255a3VjZmVwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NzQwNTg3MywiZXhwIjoyMDYyOTgxODczfQ.uopBjNwEf27Qfi-vW4tLLNEHqBDp0Lq0OiVeM2TSsvU"
   return createClient(supabaseUrl, supabaseKey)
 }
 
@@ -13,7 +15,7 @@ let clientSupabaseInstance: ReturnType<typeof createClient> | null = null
 export const createClientSupabaseClient = () => {
   if (clientSupabaseInstance) return clientSupabaseInstance
 
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ""
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://vefhpdjiqxugnykucfep.supabase.co"
   const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ""
 
   clientSupabaseInstance = createClient(supabaseUrl, supabaseKey)
