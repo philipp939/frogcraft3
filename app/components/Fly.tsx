f82c88026e2efcae314625fb2f37df7bed825ec2
philipp939 <schneiderphilipp27@icloud.com>
2025-06-21 23:20:52 +0200

    Delete app directory

--- app/components/Fly.tsx
+++
@@ -1,8 +0,0 @@
-export const Fly = ({ position }) => {
-  return (
-    <div
-      className="absolute w-3 h-3 bg-black rounded-full"
-      style={{ left: `${position.x}%`, top: `${position.y}%` }}
-    ></div>
-  )
-}
--- app/components/Score.tsx
+++
@@ -1,3 +0,0 @@
-export const Score = ({ score }) => {
-  return <div className="absolute top-4 left-4 text-2xl font-bold">Score: {score}</div>
-}
